e6ef47ba37835871fe08ac0ee0292cfe523d9f61
TranThiMili <mili.cnttk38c@gmail.com>
2018-10-04 21:27:16 +0700

    da sua

---
+++ app/src/main/java/com/example/mili/bai5/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mili.bai5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
